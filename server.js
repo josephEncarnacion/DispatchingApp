d2bab00524dbbe7f86f6ee167e8be6f0f0f5fbd6
joseph <josephencarnacion62@gmail.com>
2024-11-19 19:27:45 +0800

    .

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ const bcrypt = require('bcrypt'); // Import bcrypt
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // Use extended to parse nested objects
-app.use(cors());
-
+app.use(cors({
+  origin: 'https://dispatchingapp-1.onrender.com', // Replace with your frontend's URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],   // Add methods as needed
+  credentials: true                            // Enable cookies if required
+}));
 const responseTeamLocations = {};
 const SALT_ROUNDS = 10; // Define the number of salt rounds for bcrypt hashing
 
